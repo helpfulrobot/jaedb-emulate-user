394a44acc5a412cbd972ad4b9835bbd371ba109b
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:46:43 +1300

    Converted to PSR-2

--- code/EmulateUser_BetterButton.php
+++ code/EmulateUser_BetterButton.php
@@ -4,25 +4,29 @@
  * The button that gets injected into the Member object
  * Requires BetterButtons
  **/
-class EmulateUser_BetterButton extends BetterButtonAction {
+class EmulateUser_BetterButton extends BetterButtonAction
+{
 
     protected $link;
 
-    public function __construct($text, $link) {
+    public function __construct($text, $link)
+    {
         parent::__construct($text);
         $this->link = $link;
     }
-	
-    public function getButtonLink() {
+    
+    public function getButtonLink()
+    {
         return $this->link;
     }
 
     /**
      * Gets the HTML representing the button
-	 * We've cloned this from BetterButtonAction so we can remove the 'cms-button-link' class
+     * We've cloned this from BetterButtonAction so we can remove the 'cms-button-link' class
      * @return string
      */
-    public function getButtonHTML() {
+    public function getButtonHTML()
+    {
         return
             '<a class="ss-ui-button ui-button-text-icon-primary" href="'.$this->getButtonLink().'">
 				Emulate user
--- code/EmulateUser_ContentControllerExtension.php
+++ code/EmulateUser_ContentControllerExtension.php
@@ -1,36 +1,38 @@
 <?php
-class EmulateUser_ContentControllerExtension extends Extension {
+class EmulateUser_ContentControllerExtension extends Extension
+{
 
-	private static $allowed_actions = array(
-		'emulateuser'
-	);
-	
-	public function emulateuser($request){
-		
-		// if we're not an admin, GET OUT
-		if( !Permission::check('ADMIN') ){
-			echo 'You cannot do that';
-			die();
-		}
-		
-		// get URL parameters
-		$params = $this->owner->getRequest()->params();
-		
-		// URL attribute?
-		if( !isset($params['ID']) ){
-			Requirements::css( EMULATEUSER_DIR ."/css/emulate-user.css");
-			return $this->owner->customise( array('Users' => Member::get()) )->renderWith('EmulateUserPage');
-		}
-	
-		$member = Member::get()->byID( $params['ID'] );
-		
-		if( !isset($member->ID) ){
-			echo 'Could not find user by #'. $params['ID'];
-			die();
-		}
-		
-		$member->logIn();
-		
-		return $this->owner->redirect($this->owner->Link());
-	}
+    private static $allowed_actions = array(
+        'emulateuser'
+    );
+    
+    public function emulateuser($request)
+    {
+        
+        // if we're not an admin, GET OUT
+        if (!Permission::check('ADMIN')) {
+            echo 'You cannot do that';
+            die();
+        }
+        
+        // get URL parameters
+        $params = $this->owner->getRequest()->params();
+        
+        // URL attribute?
+        if (!isset($params['ID'])) {
+            Requirements::css(EMULATEUSER_DIR ."/css/emulate-user.css");
+            return $this->owner->customise(array('Users' => Member::get()))->renderWith('EmulateUserPage');
+        }
+    
+        $member = Member::get()->byID($params['ID']);
+        
+        if (!isset($member->ID)) {
+            echo 'Could not find user by #'. $params['ID'];
+            die();
+        }
+        
+        $member->logIn();
+        
+        return $this->owner->redirect($this->owner->Link());
+    }
 }
